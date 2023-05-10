41f1724233587efca5906e2ccec5172616cb6f98
Dat <phantiendat14012002@gmail.com>
2023-05-11 01:14:07 +0700

    dat3m

--- bank_ui/src/components/Bussiness.jsx
+++ bank_ui/src/components/Bussiness.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { features } from '../constants'
+import styles, { layout } from '../styles'
+import Button from './Button'
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex items-center flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain ' />
+    </div>
+      <div className='flex-1 flex flex-col ml-3'>
+        <h4 className='font-poppins font-semibold text-[18px] text-white leading-6'>{title}</h4>
+        <p className='font-poppins font-normal text-[16px] text-dimWhite leading-6 mt-2'>{content}</p>
+      </div>
+  </div>
+)
 
 const Bussiness = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h4 className={`${styles.heading2}`}>You do the business, <br className='sm:block hidden'/> we’ll handle the money.</h4>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by building credit, 
+          earning rewards and saving money. But with hundreds of credit cards on the market.
+        </p>
+        <Button styles='mt-10'/>
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- bank_ui/src/components/Button.jsx
+++ bank_ui/src/components/Button.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({styles}) => {
     return (
-        <div>
-            Button
-        </div>
+        <button 
+            type='button' 
+            className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] rounded-md ${styles}`}
+        >
+            Get Started
+        </button>
     )
 }
 
